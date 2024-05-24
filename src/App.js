0c1c80af0d4bd511d1eaedc22324cbcc59c57e11
Pranav Pradeep <pranavpradeep786@gmail.com>
2024-05-24 14:52:10 +0530

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
     <div className="App">
-    
+    <h1 className="text-3xl font-bold underline text-red-700">
+      Hello world!
+    </h1>
     </div>
   );
 }
